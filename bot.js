6900cd7070a4ca7d631808bf30ec063f58112c93
Conor Lamb <conor7lamb@gmail.com>
2016-12-16 01:28:14 -0500

    first draft of bot done

--- bot.js
+++ bot.js
@@ -15,39 +15,80 @@ var stream = T.stream('user');
 stream.on('tweet', tweetedAt);
 
 function tweetedAt(eventMsg) {
-  console.log('got a tweet!');
-
+  //console.log('got a tweet!');
+  //Figure out contents of tweet
   var lowerCaseTweet = eventMsg.text.toLowerCase();
   var replyTo = eventMsg.in_reply_to_screen_name;
+  var tweeter = eventMsg.user.screen_name;
+  var myStatus;
 
-  if (lowerCaseTweet.includes('who\'s a good boy') || lowerCaseTweet.includes('who is a good boy')) {
-    T.post('statuses/retweet/:id', {id:eventMsg.id_str}, function (err, data, response) {
-      console.log(data);
-    });
-  } else if (replyTo === 'PupperTweets') {
+  //necessary or he tweets at all he follows
+  if(replyTo === "PupperTweets") {
 
-    //TODO: This shit worked when I had a console log here
-    // then i got a duplicate tweet error
-    //now tha shit wont work
+      //Main Functionality : Checking what to do with the tweet
+      //Check for good boy, retweets
+    if (lowerCaseTweet.includes('who\'s a good boy') || lowerCaseTweet.includes('who is a good boy') || lowerCaseTweet.includes('whos a good boy')) {
+      T.post('statuses/retweet/:id', {id:eventMsg.id_str}, function (err, data, response) {
+        //console.log(data);
+        //Check if they put some blasphemous words in after I retweeted their good boy tweet (more of an exception)
+        var trickery = checkForBlasphemies();
+        if(trickery != false) {
+          myStatus = "@"+tweeter+" *grrrrr* "+trickery;
+          postStatus(myStatus);
+        }
+      });
+    //Check for regular replies
+    } else {
+      //check for blasphemies
+      word = checkForBlasphemies();
+      //if word == false there is no blasphemies
+      if (word == false) {
+        //Most common case, a modulus of borks  
+        myStatus = borkStatus();
+      } else {
+        //there are blasphemies
+        myStatus = "@"+tweeter+" heckin "+word+", you're doin me a "+verb()+"!";
+      }
+      postStatus(myStatus); //need 2 of these because of rts
+    }
+  }
+  //makes a bork status based on a modulus of the username
+  function borkStatus() {
+    var numBorks = (lowerCaseTweet.length % tweeter.length);
+    var borkMod = "bork"; //to make sure we don't get 0 borks
+    for (var x = 0; x < numBorks; x++)
+    {
+      borkMod.concat(" bork");
+    }
+    var uncutTweet = "@"+tweeter+" "+borkMod;
 
-    var tweeter = eventMsg.user.screen_name;
-    var word;
-    if (lowerCaseTweet.includes('cat')) {
+    if (uncutTweet.length >= 140) {         //if by some chance there are much to much borks
+      return uncutTweet.substring(0,139);
+    } else {
+      return uncutTweet;
+    }
+  }
+  //checks for words doggos hate
+  function checkForBlasphemies(){
+    var word = false;
+    if (lowerCaseTweet.includes('cat') || lowerCaseTweet.includes('kitt')) {
       word = "cats";
     } else if (lowerCaseTweet.includes('vacuum')) {
       word = 'vacuums';
     } else if (lowerCaseTweet.includes('bad boy')) {
       word = "bad boy!";
     }
-
-    var myStatus = "\"@"+tweeter+"heckin "+word+" you're doin me a "+verb()+"!\"";
-    console.log(myStatus);
+    return word;
+  }
+  //post status
+  function postStatus(myStatus) {
     T.post('statuses/update', {status:myStatus}, function (err, data, response) {
-    console.log(data);
-  });
+    //console.log(data);
+    });
   }
 }
 
+//create a verb for the frightened doggo
 function verb() {
   var chance = Math.random();
   if (chance < 0.34)
